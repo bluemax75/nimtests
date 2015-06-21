818add704f7a83ecd7a86fb7506088adf0f022e8
Carlos Miranda <charly@localhost.localdomain>
2015-06-21 10:37:01 -0300

    Agregado de ejemplo de threads
    Usa createThread y Channel para comunicacion entre threads

---
+++ thread_test/thread_test.nim
@@ -0,0 +1,23 @@
+import strutils
+
+type
+  StringChannel = TChannel[string]
+
+var
+  channels : array[0..3, StringChannel]
+  thr: array [0..3, TThread[ptr StringChannel]]
+
+proc consumer(channel: ptr StringChannel) {.thread.} =
+  echo channel[].recv()
+  channel[].send("fighters")
+
+proc main =
+  for ix in 0..3: channels[ix].open()
+  for ix in 0..3: createThread(thr[ix], consumer, addr(channels[ix]))
+  for ix in 0..3: channels[ix].send("foo (" & intToStr(ix) & ")")
+  joinThreads(thr)
+  for ix in 0..3: echo channels[ix].recv()
+  for ix in 0..3: channels[ix].close()
+
+when isMainModule:
+  main()
